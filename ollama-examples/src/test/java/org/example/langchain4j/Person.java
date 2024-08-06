c81775d7ef23bbd72fb29b54339ffcda92d18a89
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2024-08-06 16:10:43 +0900

    Extractor

--- ollama-examples/src/test/java/org/example/langchain4j/Person.java
+++ ollama-examples/src/test/java/org/example/langchain4j/Person.java
@@ -2,4 +2,24 @@ package org.example.langchain4j;
 
 public class Person {
 
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
--- ollama-examples/src/test/java/org/example/langchain4j/PersonExtractorTest.java
+++ ollama-examples/src/test/java/org/example/langchain4j/PersonExtractorTest.java
@@ -3,37 +3,24 @@ package org.example.langchain4j;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.ollama.OllamaChatModel;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(ElapsedTimeExtension.class)
 class PersonExtractorTest {
 
-    @Test
-    void simple_example() {
+//    @Test
+//    void extractPerson() {
+//
+//        ChatLanguageModel model = OllamaChatModel.builder()
+//                .baseUrl(Constants.BASE_URL)
+//                .modelName(Constants.MODEL_NAME)
+//                .build();
+//
+//        long start = System.currentTimeMillis();
+//        String answer = model.generate("Provide 3 short bullet points explaining why Java is awesome");
+//
+//        System.out.println(answer);
+//        System.out.println("Time: " + (System.currentTimeMillis() - start) + " ms");
+//    }
 
-        ChatLanguageModel model = OllamaChatModel.builder()
-                .baseUrl(Constants.BASE_URL)
-                .modelName(Constants.MODEL_NAME)
-                .build();
-
-        long start = System.currentTimeMillis();
-        String answer = model.generate("Provide 3 short bullet points explaining why Java is awesome");
-
-        System.out.println(answer);
-        System.out.println("Time: " + (System.currentTimeMillis() - start) + " ms");
-    }
-
-    @Test
-    void json_output_example() {
-
-        ChatLanguageModel model = OllamaChatModel.builder()
-                .baseUrl(Constants.BASE_URL)
-                .modelName(Constants.MODEL_NAME)
-                .format("json")
-                .build();
-
-        long start = System.currentTimeMillis();
-        String json = model.generate("Give me a JSON with 2 fields: name and age of a John Doe, 42");
-
-        System.out.println(json);
-        System.out.println("Time: " + (System.currentTimeMillis() - start) + " ms");
-    }
 }
