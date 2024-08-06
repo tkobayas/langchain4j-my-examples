20f9baaad5446da7c2de4fd5e0d40ae390efee04
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2024-08-06 17:04:37 +0900

    easy rag example

--- openai-examples/src/test/java/org/example/langchain4j/OpenAiChatModelTest.java
+++ openai-examples/src/test/java/org/example/langchain4j/OpenAiChatModelTest.java
@@ -10,11 +10,27 @@ class OpenAiChatModelTest {
     @Test
     void simple_example() {
 
-        ChatLanguageModel model = OpenAiChatModel.withApiKey("demo");
+        ChatLanguageModel model = OpenAiChatModel.builder()
+                .apiKey("demo")
+                .logRequests(true)
+                .logResponses(true)
+                .build();
 
         String joke = model.generate("Tell me a joke about Java");
 
         System.out.println(joke);
     }
 
+    @Test
+    void generateDrl() {
+
+        ChatLanguageModel model = OpenAiChatModel.withApiKey("demo");
+
+        String drl = model.generate("Write DRL rules for Drools. " +
+                                            "If a person is a child, then they are not eligible to vote. If a person is not a child, then they are eligible to vote." +
+                                            "`eligible to vote` is a flag in Person class. Modify it in RHS of the rule" +
+                                            "Don't include any other description. Write DRL rules only.");
+
+        System.out.println(drl);
+    }
 }
